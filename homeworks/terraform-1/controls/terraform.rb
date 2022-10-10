a7d11f0906f1659446470b375e82321bc213c32b
Alexander Loktionov <loktionovam@gmail.com>
2022-10-10 17:21:44 +0400

    DO-41 Fix tflint output

--- homeworks/terraform-1/controls/terraform.rb
+++ homeworks/terraform-1/controls/terraform.rb
@@ -18,7 +18,7 @@ control 'terraform' do
   end
 
   describe command('cd terraform && tflint --var-file=terraform.tfvars.example') do
-    its('stdout') { should match "Your code is following the best practices" }
+    its('stdout') { should match "" }
     its('stderr') { should eq '' }
     its('exit_status') { should eq 0 }
   end
